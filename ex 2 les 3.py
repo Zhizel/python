91cae612b9508eebbfda0b7a15a2ccb22c846a61
Zhizel <naturel@bk.ru>
2020-11-12 17:03:43 +0300

    Задание 2 к уроку 3

--- ex 2 les 3.py
+++ ex 2 les 3.py
@@ -5,5 +5,5 @@ city = input('Введите город проживания: ')
 e_mail = input('Введите e-mail: ')
 phone = input('Введите номер телефона: ')
 def user (name, surname, b_day, city, e_mail, phone):
-    return f'Имя: {name}', f"Фамилия: {surname}", f'День рождения: {b_day}', f'Город проживания: {city}', f'Электронная почта: {e_mail}', f'Телефон: {phone}'
+    return f'Имя: {name}', f'Фамилия: {surname}', f'День рождения: {b_day}', f'Город проживания: {city}', f'Электронная почта: {e_mail}', f'Телефон: {phone}'
 print ('Данные о пользователе одной строкой:', user (name, surname, b_day, city, e_mail, phone))
