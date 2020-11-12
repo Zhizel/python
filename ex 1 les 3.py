dee80f0d46318fc6c0ab18c4b1347026824fbbf3
Zhizel <naturel@bk.ru>
2020-11-12 19:53:50 +0300

    Задание 1 к уроку 3

--- ex 1 les 3.py
+++ ex 1 les 3.py
@@ -1,9 +1,11 @@
-a1=int(input("Введите делимое : "))
-a2=int(input("Введите делитель : "))
 def division(a1, a2):
     try:
         return a1/a2
     except ZeroDivisionError as err:
         print( 'Ошибка! Деление на ноль недопустимо')
+
+
+a1=int(input("Введите делимое : "))
+a2=int(input("Введите делитель : "))
 
 print('Результат деления: ', division(a1, a2))
