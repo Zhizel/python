d30051a73126e5e8ef81b796cad5284cf3794f3c
Zhizel <naturel@bk.ru>
2020-11-12 19:54:19 +0300

    Задание 2 к уроку 3

--- ex 2 les 3.py
+++ ex 2 les 3.py
@@ -1,9 +1,15 @@
+def user (name, surname, b_day, city, e_mail, phone):
+    Rezult='Имя: ' + f'{name}' + ' Фамилия: ' + f'{surname}' + ' День рождения: ' + f'{b_day}' + ' Город проживания: ' + f'{city}' + ' Электронная почта: ' + f'{e_mail}' + ' Телефон: ' + f'{phone}'
+    return Rezult
+
+
+
 name=input('Введите ваше имя: ')
 surname=input('Введите вашу фамилию: ')
 b_day = input('Дату рождения: ')
 city = input('Введите город проживания: ')
 e_mail = input('Введите e-mail: ')
 phone = input('Введите номер телефона: ')
-def user (name, surname, b_day, city, e_mail, phone):
-    return f'Имя: {name}', f'Фамилия: {surname}', f'День рождения: {b_day}', f'Город проживания: {city}', f'Электронная почта: {e_mail}', f'Телефон: {phone}'
+
+
 print ('Данные о пользователе одной строкой:', user (name, surname, b_day, city, e_mail, phone))
