7277d59db2aab9f0411d25869afecd93fdf6aef9
Ann McAshin <52403118+Zhizel@users.noreply.github.com>
2020-11-13 08:51:18 +0300

    Delete ex 2 less 2.py

--- ex 2 less 2.py
+++
@@ -1,5 +0,0 @@
-user_list = list (input('Введите ряд чисел: '))
-print(f'Ваш первичный список: {user_list}')
-for a in range(1, len(user_list), 2):
-    user_list[a - 1], user_list[a] = user_list[a], user_list[a-1]
-print(f'Измененный список: {user_list}')
